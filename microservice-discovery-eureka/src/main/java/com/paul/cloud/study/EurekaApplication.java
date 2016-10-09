bf57a02f3887efdebc4f7abbf5162c734f9a34a7
PaulZhang <zhangpeng@unisound.com>
2016-10-09 10:49:48 +0800

    服务发现

---
+++ microservice-discovery-eureka/src/main/java/com/paul/cloud/study/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.paul.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 使用Eureka做服务发现
+ * <p>
+ * Created by PaulZhang on 2016/10/9.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
