b62454929f15cc53b9a627d3d6cafc2b2cf3288f
PaulZhang <zhangpeng@unisound.com>
2016-10-09 14:16:12 +0800

    Hystrix Dashboard

---
+++ microservice-hystrix-dashboard/src/main/java/com/paulzhang/cloud/study/HystrixDashboardApplication.java
@@ -0,0 +1,17 @@
+package com.paulzhang.cloud.study;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * Created by PaulZhang on 2016/10/9.
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(HystrixDashboardApplication.class).web(true).run(args);
+    }
+}
