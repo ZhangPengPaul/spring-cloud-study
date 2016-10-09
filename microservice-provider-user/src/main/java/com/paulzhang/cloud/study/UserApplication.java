f07f4c78f9f2716d96440e72a4e6d3d6eb9c016b
PaulZhang <zhangpeng@unisound.com>
2016-10-09 12:56:17 +0800

    provider服务

---
+++ microservice-provider-user/src/main/java/com/paulzhang/cloud/study/UserApplication.java
@@ -0,0 +1,17 @@
+package com.paulzhang.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by PaulZhang on 2016/10/9.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
